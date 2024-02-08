2c53665f443462afaa55d06ec550ae1829a95761
Simon Loir <simon-loir@hotmail.com>
2024-02-08 15:44:59 +0100

    basic project structure

---
+++ packages/eslint-config/library.js
@@ -0,0 +1,38 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'prettier',
+        'eslint-config-turbo',
+        require.resolve('./base.js'),
+    ],
+    globals: {
+        React: true,
+        JSX: true,
+    },
+    env: {
+        node: true,
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {
+                project,
+            },
+        },
+    },
+    ignorePatterns: [
+        // Ignore dotfiles
+        '.*.js',
+        'node_modules/',
+        'dist/',
+    ],
+    overrides: [
+        {
+            files: ['*.js?(x)', '*.ts?(x)'],
+        },
+    ],
+};
