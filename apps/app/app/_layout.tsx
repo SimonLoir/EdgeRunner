9afaed8e125ea731fc2f6637217f8613e1973490
Simon Loir <simon-loir@hotmail.com>
2024-02-21 16:18:28 +0100

    working dark theme

--- apps/app/app/_layout.tsx
+++ apps/app/app/_layout.tsx
@@ -1,13 +1,23 @@
 import { Slot } from 'expo-router';
-import { SafeAreaView } from 'react-native';
+import { Platform, SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 
 // Import your global CSS file
 import '../global.css';
 
 export default function Layout() {
     return (
-        <SafeAreaView>
+        <SafeAreaView
+            style={safeAreaStyle.AndroidSafeArea}
+            className='bg-[rgb(24,24,24)]'
+        >
             <Slot />
         </SafeAreaView>
     );
 }
+
+const safeAreaStyle = StyleSheet.create({
+    AndroidSafeArea: {
+        flex: 1,
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    },
+});
