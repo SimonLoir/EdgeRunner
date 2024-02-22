291e9fe96e7c1258ea0683d5c89e817aa63e2cd0
Simon Loir <simon-loir@hotmail.com>
2024-02-22 11:23:54 +0100

    added basic express server + typescript + eslint

---
+++ packages/eslint-config/express.js
@@ -0,0 +1,29 @@
+const { resolve } = require('node:path');
+
+const project = resolve(process.cwd(), 'tsconfig.json');
+
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'prettier',
+        'eslint-config-turbo',
+        require.resolve('./base.js'),
+    ],
+    env: {
+        node: true,
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {
+                project,
+            },
+        },
+    },
+    ignorePatterns: [
+        // Ignore dotfiles
+        '.*.js',
+        'node_modules/',
+    ],
+    overrides: [{ files: ['*.js?(x)', '*.ts?(x)'] }],
+};
