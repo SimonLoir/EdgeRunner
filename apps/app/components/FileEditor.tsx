2e80d6d34fa6a388c926e76101ea701f5bbf682e
JohanRochet <91260794+JohanRochet@users.noreply.github.com>
2024-04-11 16:05:15 +0200

    better abortcontroller

--- apps/app/components/FileEditor.tsx
+++ apps/app/components/FileEditor.tsx
@@ -14,6 +14,7 @@ import getLastWordFromCharPos from 'utils/getLastWordFromCharPos';
 import { z } from 'zod';
 import { completionItemSchema } from '@/schemas/exportedSchemas';
 import { KeyboardContext } from '../utils/keyboardContext';
+const ac = new AbortController();
 
 export default function FileEditor({ file }: { file: string }) {
     const workspace = useWorkspace();
@@ -22,7 +23,6 @@ export default function FileEditor({ file }: { file: string }) {
         workspace.getFileContent(file)
     );
     const [version, setVersion] = useState<number>(0);
-    const ac = new AbortController();
 
     if (file === undefined || typeof file !== 'string')
         throw new Error('file is required and must be a string');
