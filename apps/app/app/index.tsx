2d654a882a9befc52c44160cacf97c110556be10
JohanRochet <91260794+JohanRochet@users.noreply.github.com>
2024-04-11 16:22:01 +0200

    color for status bar

--- apps/app/app/index.tsx
+++ apps/app/app/index.tsx
@@ -13,6 +13,7 @@ import { z } from 'zod';
 import { completionItemSchema } from '@/schemas/exportedSchemas';
 import { KeyboardContext } from '../utils/keyboardContext';
 import { Gesture, GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
 
 const queryClient = new QueryClient();
 
@@ -30,68 +31,71 @@ export default function IndexPage() {
     >([]);
 
     return (
-        <GestureHandlerRootView style={{ flex: 1 }}>
-            <KeyboardContext.Provider
-                value={{
-                    isKeyboardOpen,
-                    setIsKeyboardOpen,
-                    keyboardItems,
-                    setKeyboardItems,
-                    enableNativeKeyboard,
-                    setEnableNativeKeyboard,
-                }}
-            >
-                <trpc.Provider
-                    queryClient={queryClient}
-                    client={trpcReactClient}
+        <>
+            <StatusBar style='light' backgroundColor='rgb(40,40,40)' />
+            <GestureHandlerRootView style={{ flex: 1 }}>
+                <KeyboardContext.Provider
+                    value={{
+                        isKeyboardOpen,
+                        setIsKeyboardOpen,
+                        keyboardItems,
+                        setKeyboardItems,
+                        enableNativeKeyboard,
+                        setEnableNativeKeyboard,
+                    }}
                 >
-                    <QueryClientProvider client={queryClient}>
-                        <WorkspaceContext.Provider value={workspace}>
-                            <WorkspaceInitializer />
-                            <Scaffold>
-                                <Scaffold.ActivityBar>
-                                    <Scaffold.ActivityBar.Group>
+                    <trpc.Provider
+                        queryClient={queryClient}
+                        client={trpcReactClient}
+                    >
+                        <QueryClientProvider client={queryClient}>
+                            <WorkspaceContext.Provider value={workspace}>
+                                <WorkspaceInitializer />
+                                <Scaffold>
+                                    <Scaffold.ActivityBar>
+                                        <Scaffold.ActivityBar.Group>
+                                            <Scaffold.ActivityBar.Item
+                                                iconName='folder-outline'
+                                                page='file-explorer'
+                                            />
+                                            <Scaffold.ActivityBar.Item
+                                                iconName='folder-outline'
+                                                page='symbols-explorer'
+                                            />
+                                        </Scaffold.ActivityBar.Group>
                                         <Scaffold.ActivityBar.Item
-                                            iconName='folder-outline'
-                                            page='file-explorer'
+                                            iconName='settings-outline'
+                                            goTo='/settings'
                                         />
-                                        <Scaffold.ActivityBar.Item
-                                            iconName='folder-outline'
-                                            page='symbols-explorer'
-                                        />
-                                    </Scaffold.ActivityBar.Group>
-                                    <Scaffold.ActivityBar.Item
-                                        iconName='settings-outline'
-                                        goTo='/settings'
-                                    />
-                                </Scaffold.ActivityBar>
-                                <Scaffold.SidePanel>
-                                    <Scaffold.SidePanel.Page name='file-explorer'>
-                                        <FileExplorer />
-                                    </Scaffold.SidePanel.Page>
-                                    <Scaffold.SidePanel.Page name='symbols-explorer'>
-                                        <SymbolsExplorer />
-                                    </Scaffold.SidePanel.Page>
-                                </Scaffold.SidePanel>
-                                <Scaffold.Main />
-                            </Scaffold>
-                        </WorkspaceContext.Provider>
-                    </QueryClientProvider>
-                </trpc.Provider>
+                                    </Scaffold.ActivityBar>
+                                    <Scaffold.SidePanel>
+                                        <Scaffold.SidePanel.Page name='file-explorer'>
+                                            <FileExplorer />
+                                        </Scaffold.SidePanel.Page>
+                                        <Scaffold.SidePanel.Page name='symbols-explorer'>
+                                            <SymbolsExplorer />
+                                        </Scaffold.SidePanel.Page>
+                                    </Scaffold.SidePanel>
+                                    <Scaffold.Main />
+                                </Scaffold>
+                            </WorkspaceContext.Provider>
+                        </QueryClientProvider>
+                    </trpc.Provider>
 
-                <CodeKeyboard
-                    onDismiss={() => {
-                        setIsKeyboardOpen(false);
-                    }}
-                    isVisble={isKeyboardOpen}
-                    onOpen={() => {
-                        setEnableNativeKeyboard(false);
-                        setIsKeyboardOpen(true);
-                    }}
-                    isNativeKeyboardEnabled={enableNativeKeyboard}
-                    keyBoardItems={keyboardItems}
-                />
-            </KeyboardContext.Provider>
-        </GestureHandlerRootView>
+                    <CodeKeyboard
+                        onDismiss={() => {
+                            setIsKeyboardOpen(false);
+                        }}
+                        isVisble={isKeyboardOpen}
+                        onOpen={() => {
+                            setEnableNativeKeyboard(false);
+                            setIsKeyboardOpen(true);
+                        }}
+                        isNativeKeyboardEnabled={enableNativeKeyboard}
+                        keyBoardItems={keyboardItems}
+                    />
+                </KeyboardContext.Provider>
+            </GestureHandlerRootView>
+        </>
     );
 }
