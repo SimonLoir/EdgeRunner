427197c24c1be692762d996b13d9aa8ce8d53a81
JohanRochet <91260794+JohanRochet@users.noreply.github.com>
2024-04-22 16:08:37 +0200

    fix bug with opening keyboad on load + close keyboard on refactor mode

--- apps/app/components/EditModeSwitcher.tsx
+++ apps/app/components/EditModeSwitcher.tsx
@@ -2,16 +2,21 @@ import useEditMode from '../utils/workspace/hooks/useEditMode';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native';
 import useWorkspace from '../utils/workspace/hooks/useWorkspace';
+import { KeyboardContext } from 'utils/keyboardContext';
+import { useContext } from 'react';
 
 export default function EditModeSwitcher() {
     const workspace = useWorkspace();
     const editMode = useEditMode();
+
+    const keyboardContext = useContext(KeyboardContext);
     return (
         <TouchableOpacity
             className='absolute bottom-2 right-2 size-14 rounded-full items-center justify-center bg-[rgb(50,50,50)]'
             onPress={() => {
                 if (editMode === 'text') {
                     workspace.editMode = 'refactor';
+                    keyboardContext.setIsKeyboardOpen(false);
                 } else {
                     workspace.editMode = 'text';
                 }
